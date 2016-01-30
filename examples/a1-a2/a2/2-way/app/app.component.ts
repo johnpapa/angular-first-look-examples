9b44330db2625c3ce111eda6ff427843e4a3f4e3
John Papa <john@johnpapa.net>
2016-01-30 17:39:57 -0500

    2 way binding

---
+++ examples/a1-a2/a2/2-way/app/app.component.ts
@@ -0,0 +1,13 @@
+//our root app component
+import {Component} from 'angular2/core'
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app/app.component.html',
+})
+export class AppComponent {
+  title = 'Angular 2 Two-Way Binding';
+  story = {
+    name: 'The Empire Strikes Back'
+  };
+}
