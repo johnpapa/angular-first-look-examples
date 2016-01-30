46287f95d363865c69418e49fa57240ffbb9648a
John Papa <john@johnpapa.net>
2016-01-30 17:22:23 -0500

    moved binding events to a2 folder

--- examples/binding-events/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrap } from 'angular2/platform/browser';
-import { AppComponent } from './app.component';
-
-bootstrap(AppComponent, [])
-  .then(success => console.log(`Bootstrap success`))
-  .catch(error => console.log(error));
