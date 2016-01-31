1ab8f22dbdd94630b089350990555c57c31ec711
John Papa <john@johnpapa.net>
2016-01-30 19:08:40 -0500

    updated to beta.2

---
+++ examples/a1-a2/a2/services/app/vehicle.service.ts
@@ -0,0 +1,10 @@
+import {Injectable} from 'angular2/core';
+
+@Injectable()
+export class VehicleService {
+  getVehicles = () => [
+    { id: 1, name: 'X-Wing Fighter' },
+    { id: 2, name: 'Tie Fighter' },
+    { id: 3, name: 'Y-Wing Fighter' }
+  ];
+}
---
+++ examples/a1-a2/a2/services/app/vehicles.component.ts
@@ -0,0 +1,16 @@
+import { Component } from 'angular2/core';
+
+import { VehicleService } from './vehicle.service';
+
+@Component({
+  selector: 'my-vehicles',
+  templateUrl: 'app/vehicles.component.html',
+  providers: [VehicleService]
+})
+export class VehiclesComponent {
+  constructor(private _vehicleService: VehicleService) {}
+  vehicles = this._vehicleService.getVehicles();
+}
+
+
+
