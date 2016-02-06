b48d05f7cc50f4ba36dd5b0fb952d8c7c6f12044
John Papa <john@johnpapa.net>
2016-02-06 15:40:29 -0500

    data binding example

--- examples/data-binding/app/app.component.ts
+++ examples/data-binding/app/app.component.ts
@@ -1,24 +1,17 @@
 import { Component } from 'angular2/core';
 import { HTTP_PROVIDERS } from 'angular2/http';
 
-import { CharactersComponent } from './characters.component';
+import { CharacterComponent } from './character.component';
 
 @Component({
   selector: 'story-app',
   template: `
   <div>
-    <h1>Storyline Tracker</h1>
-    <h3>Component Demo</h3>
-    <story-characters [storyId]="7" (changed)=changed($event)></story-characters>
+    <h3>Storyline Tracker - Data Binding Demo</h3>
+    <story-character></story-character>
   </div>
   `,
-  directives: [CharactersComponent],
+  directives: [CharacterComponent],
   providers: [HTTP_PROVIDERS]
 })
-export class AppComponent {
-  changed(changedCharacter: any) {
-    if (changedCharacter) {
-      console.log(`Event Emitter said you selected ${changedCharacter.name}`);
-    }
-  }
-}
+export class AppComponent {}
--- examples/data-binding/app/character.component.ts
+++ examples/data-binding/app/character.component.ts
@@ -1,13 +1,44 @@
-  import { Component, Input } from 'angular2/core';
+import { Component } from 'angular2/core';
 
-  import { Character } from './character.service';
+class Character {
+  constructor(public id: number, public name: string, public side: string, public imageUrl: string, public link: string) { }
+}
+class Vehicle {
+  constructor(public id: number, public name: string) { }
+}
 
-  @Component({
-    selector: 'story-character',
-    template: '<h3 *ngIf="character">You selected {{character.name}}</h3>',
-  })
-  export class CharacterComponent {
-    @Input() character: Character;
+@Component({
+  selector: 'story-character',
+  templateUrl: './app/character.component.html',
+  styleUrls: ['./app/character.component.css']
+})
+export class CharacterComponent {
+  character: Character;
+  color = '';
+  isSelected = false;
+  selectLabel = 'Select a Character';
+  vehicles = [
+    new Vehicle(1, 'Slave 1'),
+    new Vehicle(2, 'Imperial Star Destroyer'),
+    new Vehicle(3, 'Escape Pod')
+  ];
+
+  constructor() {
+    this.character = new Character(100, 'Boba Fett', 'dark', 'assets/man.png', 'http://angular.io');
   }
 
+  getClasses() {
+    let classes: string[] = ['bordered'];
+    if (this.isSelected) {
+      classes.push('selected');
+    }
+    return classes.join(' ');
+  }
 
+  select(name: string) {
+    let msg = `You selected ${name}`;
+    console.log(msg);
+    this.isSelected = !this.isSelected;
+  }
+
+}
