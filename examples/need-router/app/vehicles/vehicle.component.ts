5877046c24426783254407511f526026613c1715
John Papa <john@johnpapa.net>
2016-02-14 18:52:28 -0500

    removed more code form needs-router

--- examples/need-router/app/vehicles/vehicle.component.ts
+++ examples/need-router/app/vehicles/vehicle.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input, OnInit } from 'angular2/core';
 
-import { Vehicle, VehicleService } from '../vehicles/vehicle.service';
-
 @Component({
   selector: 'story-vehicle',
   templateUrl: 'app/vehicles/vehicle.component.html'
@@ -9,14 +7,8 @@ import { Vehicle, VehicleService } from '../vehicles/vehicle.service';
 export class VehicleComponent implements OnInit {
   @Input() vehicle: Vehicle;
 
-  constructor(
-    private _vehicleService: VehicleService) { }
+  constructor() { }
 
   ngOnInit() {
-    if (!this.vehicle) {
-      // let id = +this._routeParams.get('id');
-      this._vehicleService.getVehicle(id)
-        .subscribe((vehicle: Vehicle) => this.vehicle = vehicle);
-    }
   }
 }
