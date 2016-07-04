a12aa1d669177199dcfb5573097e92743a6c6902
John Papa <john@johnpapa.net>
2016-07-04 19:29:19 -0400

    fixed main.ts

--- examples/storyline-tracker/main.ts
+++ examples/storyline-tracker/main.ts
@@ -2,12 +2,15 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 
 import { AppComponent } from './app/app.component';
+import { APP_ROUTER_PROVIDERS } from './app/app.routes';
 import { environment } from './app/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent)
+bootstrap(AppComponent, [
+  APP_ROUTER_PROVIDERS
+])
   .then(success => console.log(`Bootstrap success`))
   .catch(error => console.log(error));
