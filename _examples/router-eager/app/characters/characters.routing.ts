75fd8871526949fcb6797170d59a02a8e89f1c21
John Papa <john@johnpapa.net>
2016-09-02 22:56:21 -0400

    fixed forroot --> forchild (copy/paste)

--- _examples/router-eager/app/characters/characters.routing.ts
+++ _examples/router-eager/app/characters/characters.routing.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class CharactersRouterModule { }
--- _examples/router-eager/app/vehicles/vehicles.routing.ts
+++ _examples/router-eager/app/vehicles/vehicles.routing.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class VehiclesRoutingModule { }
