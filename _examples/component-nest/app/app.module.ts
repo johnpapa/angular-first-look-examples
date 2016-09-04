29b8c26a7e4570bcfcb3fb3ba72f57e77b6b649a
John Papa <john@johnpapa.net>
2016-09-03 21:22:14 -0400

    old code

--- _examples/component-nest/app/app.module.ts
+++ _examples/component-nest/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { CharacterComponent } from './character.component';
 import { CharacterListComponent } from './character-list.component';
