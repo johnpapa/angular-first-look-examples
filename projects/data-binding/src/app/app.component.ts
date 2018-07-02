6dfc4919146275af92f1e56aacd6923374752fbd
John L Papa <john@johnpapa.net>
2018-07-02 15:23:23 -0400

    data binding

--- projects/data-binding/src/app/app.component.ts
+++ projects/data-binding/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'story-app',
+  template: `
+  <div>
+    <h3>Storyline Tracker - Data Binding Demo</h3>
+
+    <div style="margin:1em">
+      <button class="dashboard-button mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect
+      mdl-button--accent"
+        (click)="solve()">{{buttonText}}</button>
+    </div>
+
+    <span [ngSwitch]="showSolution">
+      <template [ngSwitchCase]="true">
+        <story-character-solved></story-character-solved>
+      </template>
+      <template ngSwitchDefault>
+        <story-character></story-character>
+      </template>
+    </span>
+  </div>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  buttonText = 'Switch to Solution';
+  showSolution = false;
+
+  solve() {
+    this.showSolution = !this.showSolution;
+    this.buttonText = this.showSolution
+      ? 'Switch to  Starter'
+      : 'Switch to Solution';
+  }
 }
--- projects/data-binding/src/app/app.module.ts
+++ projects/data-binding/src/app/app.module.ts
@@ -1,16 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CharacterComponent } from './character.component';
+import { CharacterSolvedComponent } from './solution/character-solved.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent, CharacterComponent, CharacterSolvedComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
