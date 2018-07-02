a6f218a0bc42418e13fd160afec565cf46348b8a
John L Papa <john@johnpapa.net>
2018-07-02 16:40:31 -0400

    needs http

--- projects/need-http/src/app/app.component.ts
+++ projects/need-http/src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  template: '<my-vehicle-list></my-vehicle-list>'
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
--- projects/need-http/src/app/app.module.ts
+++ projects/need-http/src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { VehicleListComponent } from './vehicle-list.component';
+import { VehicleComponent } from './vehicle.component';
+import { VehicleService } from './vehicle.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, VehicleComponent, VehicleListComponent],
+  providers: [VehicleService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
