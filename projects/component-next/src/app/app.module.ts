d4f8b015ae3e9ef4c8521addc9246a4da0ea8db5
John L Papa <john@johnpapa.net>
2018-07-02 15:20:29 -0400

    comp next

--- projects/component-next/src/app/app.module.ts
+++ projects/component-next/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule  } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { CharacterComponent } from './character.component';
+import { CharacterListComponent } from './character-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    CharacterComponent,
+    CharacterListComponent
+  ],
+  bootstrap: [CharacterListComponent],
 })
 export class AppModule { }
