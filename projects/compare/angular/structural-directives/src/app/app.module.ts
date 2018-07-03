8f15645b2affb9d2cf58667edd19059473224aeb
John L Papa <john@johnpapa.net>
2018-07-03 08:19:29 -0400

    more

--- projects/compare/angular/structural-directives/src/app/app.module.ts
+++ projects/compare/angular/structural-directives/src/app/app.module.ts
@@ -1,16 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { VehiclesComponent } from './vehicles.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule],
+  declarations: [VehiclesComponent],
+  bootstrap: [VehiclesComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ projects/compare/angular/structural-directives/src/app/vehicles.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-vehicles',
+  templateUrl: './vehicles.component.html'
+})
+export class VehiclesComponent {
+  vehicles = [
+    { id: 1, name: 'X-Wing Fighter' },
+    { id: 2, name: 'Tie Fighter' },
+    { id: 3, name: 'Y-Wing Fighter' }
+  ];
+}
