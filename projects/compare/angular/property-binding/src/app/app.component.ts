3b77ce0d1800229f214aa7f8c4e8800d318dda98
John L Papa <john@johnpapa.net>
2018-07-03 08:17:26 -0400

    prop bind

--- projects/compare/angular/property-binding/src/app/app.component.ts
+++ projects/compare/angular/property-binding/src/app/app.component.ts
@@ -1,10 +1,13 @@
+// our root app component
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app';
+  imagePath = 'assets/angular.png';
+  link = 'http://angular.io';
+  story = 'The Empire Strikes Back';
+  title = 'Angular Property Binding';
 }
--- projects/compare/angular/property-binding/src/app/app.module.ts
+++ projects/compare/angular/property-binding/src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
